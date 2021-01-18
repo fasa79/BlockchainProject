39a2b88c66ddae6ae722bc885444c92ddbda5eda
mrfaeez <faexd99@gmail.com>
2021-01-18 19:18:27 +0800

    Add few functionalities for webpages

--- src/js/app.js
+++ src/js/app.js
@@ -37,7 +37,7 @@ App = {
   },
 
   initContract: function() {
-    $.getJSON('LaundererDetector.json', function(data) {
+    $.getJSON('build/contracts/LaundererDetector.json', function(data) {
       // Get the necessary contract artifact file and instantiate it with @truffle/contract
       var LaundererArtifact = data;
       App.contracts.LaundererDetector = TruffleContract(LaundererArtifact);
@@ -53,8 +53,139 @@ App = {
   },
 
   bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
+    $(document).on('click', '#btn-getBalance', App.updateBalance);
+    $(document).on('click', '#btn-deposit', App.handleDeposit);
+    $(document).on('click', '#btn-withdraw', App.handleWithdraw);
+    $(document).on('click', '#btn-checkHigh', App.handleHighTransactors);
+    $(document).on('click', '#btn-checkLaund', App.handlePotentialLaunderers);
   },
+
+  updateBalance: function() {
+    var LDInstance;
+
+    App.contracts.LaundererDetector.deployed().then(function(instance) {
+      LDInstance = instance;
+
+      return LDInstance.getBalance.call();
+    }).then(function(balance) {
+      // balance = Math.round(balance * 100.0 / 10 ** 18) / 100;
+      balance /= 10 ** 18;
+      $('#sc-balance').text(balance);
+    }).catch(function(err) {
+      console.log(err.message);
+    });
+  },
+
+  
+
+  handleDeposit: function(event) {
+    event.preventDefault();
+    
+    var LDInstance;
+    var amount = parseInt($('#amt-deposit').val());
+    
+    web3.eth.getAccounts(function(error, accounts) {
+    if (error) {
+      console.log(error);
+    }
+      var account = accounts[0];
+
+      App.contracts.LaundererDetector.deployed().then(function(instance) {
+        LDInstance = instance;
+
+        // Execute deposit as a transaction by sending account and amount
+        return LDInstance.deposit({from: account, value: amount * 10**18});
+      }).then(function(result) {
+        return App.updateBalance();
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
+  },
+
+  handleWithdraw: function(event) {
+    
+    
+    var LDInstance;
+    var amount = parseInt($('#amt-withdraw').val());
+    
+    web3.eth.getAccounts(function(error, accounts) {
+    if (error) {
+      console.log(error);
+    }
+      var account = accounts[0];
+
+      App.contracts.LaundererDetector.deployed().then(function(instance) {
+        LDInstance = instance;
+
+        // Execute deposit as a transaction by sending account and amount
+        return LDInstance.withdraw(amount, {from: account});
+      }).then(function(result) {
+        return App.updateBalance();
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
+  },
+
+  handleHighTransactors: function(event) {
+    event.preventDefault();
+    
+    var LDInstance;
+    var hugeTransactAdress;
+    
+    web3.eth.getAccounts(function(error, accounts) {
+    if (error) {
+      console.log(error);
+    }
+      var account = accounts[0];
+
+      App.contracts.LaundererDetector.deployed().then(function(instance) {
+        LDInstance = instance;
+
+        hugeTransactAdress = LDInstance.getHugeTransactors({from: account});
+
+        return LDInstance.getHugeTransactors.call();
+      }).then(function(hugeTransactAdress) {
+        
+        var tableHuge = document.getElementById("tableHuge");
+
+        for (i = 0; i < hugeTransactAdress[0].length; i++) {
+          var num = document.createTextNode(i+1);
+          var hugeAddr = hugeTransactAdress[0][i];
+          var hugeAmm = hugeTransactAdress[1][i];
+
+          hugeAmm = parseInt(hugeAmm);
+          hugeAmm /= 10 ** 18;
+
+          var hugeTag = document.createElement('tr');
+          hugeTag.innerHTML = "<td>" + (i+1) + "</td><td>" + hugeAddr + "</td><td>" + hugeAmm + "</td>"
+  
+          tableHuge.appendChild(hugeTag);
+        }
+
+
+      }).catch(function(err) {
+
+          var errorDiv = document.getElementById("error-div");
+          var errorTag = document.createElement('div');
+
+          errorTag.setAttribute('class', 'alert alert-danger');
+          errorTag.innerHTML = "Your Account is ineligible to do that!"
+        
+          errorDiv.appendChild(errorTag);
+          // console.log(err.message);
+      });
+    });
+  },
+
+  handlePotentialLaunderers: function(event) {
+
+  }
+
+  // bindEvents: function() {
+  //   $(document).on('click', '.btn-adopt', App.handleAdopt);
+  // },
 
   // markAdopted: function() {
   //   var adoptionInstance;
